c834b4bc5951219eeb553906fdabb80cd3b49589
Rafael Brayner Miranda de Carvalho <rafael.carvalho@pmjppaco.local>
2020-05-14 19:20:40 -0300

    Adiciona estilo ao load para ficar no centro da página

--- src/components/load/index.js
+++ src/components/load/index.js
@@ -4,7 +4,16 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    top: 0,
+    left: 0,
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    background: '#eee',
     display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: '2em',
     '& > * + *': {
       marginLeft: theme.spacing(2),
     },
